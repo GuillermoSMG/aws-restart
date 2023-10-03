f90c2cb09dafffca1d8c2a4fdc9835cc9d2a573b
Guillermo San Martín <guille.seba0802@gmail.com>
2023-10-02 21:00:55 -0300

    variables

---
+++ LastName_FirstName_JS_CrearVariables.js
@@ -0,0 +1,9 @@
+/* 
+Variables que afectan a la admisión en una montaña rusa.
+*/
+
+// La altura debe ser mayor o igual a 1 metro (100cm).
+let height;
+
+// La edad debe ser mayor a 10 años.
+let age;
