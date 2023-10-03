f75c6bc9adccd43e9078588160947503f5373283
Guillermo San Martín <guille.seba0802@gmail.com>
2023-10-03 16:29:47 -0300

    depuracion

---
+++ Depuracion.js
@@ -0,0 +1,20 @@
+/* function greeting() {
+  return 'Hola, mundo.';
+}
+
+var word = greeting();
+console.log(); */
+
+function greeting() {
+  return 'Hola, mundo.';
+}
+
+console.log(greeting());
+
+// Ó //
+
+function greeting() {
+  return 'Hola, mundo.';
+}
+var word = greeting();
+console.log(word);
