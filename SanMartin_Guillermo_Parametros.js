03257321ef5420c14665f52b3efae6230d4eb459
Guillermo San Martín <guille.seba0802@gmail.com>
2023-10-05 14:12:10 -0300

    parameters

---
+++ SanMartin_Guillermo_Parametros.js
@@ -0,0 +1,22 @@
+const greet = name => {
+  if (name === 'Count Dooku') {
+    console.log('¡Voy por ti, Dooku!');
+    return;
+  }
+
+  const hour = new Date().getHours();
+  const minutes = new Date().getMinutes();
+  let greeting = '';
+  if (hour < 12 && hour > 6) {
+    greeting = 'Buenos días';
+  } else if (hour > 12 && hour < 19) {
+    greeting = 'Buenas tardes';
+  } else {
+    greeting = 'Buenas noches';
+  }
+  console.log(
+    `¡${greeting}, ${name}! ${hour}:${minutes <= 9 ? '0' : ''}${minutes}`
+  );
+};
+
+greet('Guillermo');
